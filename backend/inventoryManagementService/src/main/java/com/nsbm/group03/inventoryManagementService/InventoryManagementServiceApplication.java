5838d9f3f4102c0b54b29e439b362fd3c198284d
NadeemalTM <nadeemal@example.com>
2026-01-29 16:05:41 +0530

    Add Inventory Management Service with initial setup, configuration, and basic structure

---
+++ backend/inventoryManagementService/src/main/java/com/nsbm/group03/inventoryManagementService/InventoryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.inventoryManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/inventoryManagementService/src/test/java/com/nsbm/group03/inventoryManagementService/InventoryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.inventoryManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
