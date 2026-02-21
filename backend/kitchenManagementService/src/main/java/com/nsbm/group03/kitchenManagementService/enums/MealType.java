c2fe1bb864e992b5b31d3d4cb82bc4a39223a150
Milanka08 <milankamuthuni5@gmail.com>
2026-02-21 17:53:48 +0530

    feat(kitchen): add entity models and enums for kitchen management
    - Add KitchenMenuItem, KitchenOrder, KitchenOrderItem entities
    - Add MealType (BREAKFAST, LUNCH, DINNER, BUFFET) enum
    - Add OrderStatus (PENDING, COOKING, READY, SERVED) enum
    - Add ServiceType (RESTAURANT, EVENT) enum

---
+++ backend/kitchenManagementService/src/main/java/com/nsbm/group03/kitchenManagementService/enums/MealType.java
@@ -0,0 +1,8 @@
+package com.nsbm.group03.kitchenManagementService.enums;
+
+public enum MealType {
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    BUFFET
+}
---
+++ backend/kitchenManagementService/src/main/java/com/nsbm/group03/kitchenManagementService/enums/ServiceType.java
@@ -0,0 +1,6 @@
+package com.nsbm.group03.kitchenManagementService.enums;
+
+public enum ServiceType {
+    RESTAURANT,
+    EVENT
+}
